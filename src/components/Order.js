19bf6ba73e52aa1428b213587a8c6598c9a5d65b
edalau <111753040+edalau@users.noreply.github.com>
2022-09-23 11:53:52 +0200

    new home wow

---
+++ src/components/Order.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const Order = ({ pizza }) => {
+  useEffect(() => {
+
+  });
+  return (
+    <div className="container order">
+      <h2>Thank you for your order :)</h2>
+      <p>You ordered a {pizza.base} pizza with:</p>
+      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+    </div>
+  )
+}
+
+export default Order;
