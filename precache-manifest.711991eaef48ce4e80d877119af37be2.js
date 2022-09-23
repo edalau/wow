e85a145e05f8d910a66a692b99bd73337da63c22
edalau <111753040+edalau@users.noreply.github.com>
2022-09-23 11:58:49 +0200

    Updates

---
+++ precache-manifest.711991eaef48ce4e80d877119af37be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2a6dc6e9f6f7b9701b5c17432242a1",
+    "url": "/wow/index.html"
+  },
+  {
+    "revision": "7201c1e32e888be07ffd",
+    "url": "/wow/static/css/main.dc56fa15.chunk.css"
+  },
+  {
+    "revision": "643cc5d57747d2881da3",
+    "url": "/wow/static/js/2.744d4cea.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/wow/static/js/2.744d4cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7201c1e32e888be07ffd",
+    "url": "/wow/static/js/main.e303cb4a.chunk.js"
+  },
+  {
+    "revision": "2e1a8e96203ab6b4220a",
+    "url": "/wow/static/js/runtime-main.0817d79c.js"
+  }
+]);
